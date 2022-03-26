02cf4fee35474b1383f5f942317ef0a8670bde70
Troy Neubauer <troyneubauer@gmail.com>
2022-03-26 18:31:00 -0400

    Add initial structs and run rustfmt

--- src/config.rs
+++ src/config.rs
@@ -1,2 +1,1 @@
-pub struct A {
-}
+pub struct A {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,17 @@
 pub mod audio;
 pub mod config;
+
+pub struct Config {
+    pub dials: Vec<Dial>,
+    pub z_ball_dir_change_min: f64,
+    pub z_ball_dir_change_max: f64,
+    pub y_ball_speed_min: f64,
+    pub y_ball_speed_max: f64,
+}
+
+pub struct Dial {
+    pub in_range_zone: f64,
+    pub out_of_range_zone: f64,
+    pub alarm_file: String,
+    pub unit_step: f64,
+}
